d127a51f02e038666f409981820b446ac0e5d10c
jescoevas <jescoevas@alum.us.es>
2019-11-26 22:13:34 +0100

    Entities and database completed.

--- src/main/java/acme/framework/entities/Authenticated.java
+++ src/main/java/acme/framework/entities/Authenticated.java
@@ -6,7 +6,7 @@
  * In keeping with the traditional purpose of furthering education and research, it is
  * the policy of the copyright owner to permit non-commercial use and redistribution of
  * this software. It has been tested carefully, but it is not guaranteed for any particular
- * purposes.  The copyright owner does not offer any warranties or representations, nor do
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
  * they accept any liabilities with respect to them.
  */
 
@@ -31,5 +31,11 @@ public class Authenticated extends UserRole {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+
+	//	@OneToMany
+	//	private Collection<Thread>	threadsOwned;
+	//
+	//	@ManyToMany
+	//	private Collection<Thread>	threadsInvolved;
 
 }
