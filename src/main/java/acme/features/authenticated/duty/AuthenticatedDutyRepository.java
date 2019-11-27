333906cbf716a80441e6a1ceb4db71d8907b627d
jescoevas <jescoevas@alum.us.es>
2019-11-27 18:26:06 +0100

    List and show active jobs (Authenticated)

---
+++ src/main/java/acme/features/authenticated/duty/AuthenticatedDutyRepository.java
@@ -0,0 +1,24 @@
+
+package acme.features.authenticated.duty;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.duties.Duty;
+import acme.entities.jobs.Job;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AuthenticatedDutyRepository extends AbstractRepository {
+
+	@Query("select j.duties from Job j where j.id=?1")
+	Collection<Duty> findDutiesFromJob(int id);
+
+	@Query("select j from Job j where j.id=?1")
+	Job findOneJobById(int id);
+
+	@Query("select d from Duty d where d.id=?1")
+	Duty findOneDutyById(int id);
+}
